355e31dca8b55ad0f075a376f3074152142dd882
Jason Utt <jdmagic21@gmail.com>
2019-06-28 21:51:34 -0400

    Removed logic, created new template file.

--- hbsHelpers/compile.js
+++ hbsHelpers/compile.js
@@ -3,14 +3,7 @@ const handlebars = require('handlebars');
 var exports = module.exports = {}; 
 exports.compile = (html, loops, data) =>{
     return new Promise((resolve, reject, data) =>{
-        try{
-            if(!data.length > 0){
-                html = html.replace("{{#times x}}", `{{#times ${loops}}}`);
-            }
-            else{
-                html = html.replace("{{#each x}}", `{{#each data}}`);
-                html = html.replace("{{/times}}, {{/each}}");
-            }            
+        try{                    
             const template = handlebars.compile(html)
             const result = template(data); 
             resolve(result); 
